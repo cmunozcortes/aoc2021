6f28cb6ef3c0dc4a3bd381a4d79e8a71e7240275
Chris M <christopher.munoz-cortes@analog.com>
2021-12-13 22:13:34 -0500

    Add solutions for day 2

--- day2/map_parser.py
+++ day2/map_parser.py
@@ -3,6 +3,34 @@ Advent of Code 2021 Day 2 Solutions
 """
 
 # Read input file
+x = y = 0
 with open('input.txt') as f:
     for line in f:
-        print(line)
+        direction, length = line.split()
+        if direction == "forward":
+            x += int(length)
+        if direction == "up":
+            y -= int(length)
+        if direction == "down":
+            y += int(length)
+
+# Solutions to part 1
+print(f"Final position {(x, y)}")
+print(f"x*y product: {x * y}")
+
+# Solve part 2 (new interpretation of the commands)
+aim = x = y = 0
+with open('input.txt') as f:
+    for line in f:
+        direction, length = line.split()
+        if direction == "forward":
+            x += int(length)
+            y += (aim * int(length))
+        if direction == "up":
+            aim -= int(length)
+        if direction == "down":
+            aim += int(length)
+
+# Solutions to part 2
+print(f"Final position {(x,y)}")
+print(f"xy product: {x*y}")
