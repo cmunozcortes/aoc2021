bb826e14c2730c430f9b5fdd5b988b9561d55b93
Chris Munoz <cmunozcortes@me.com>
2021-12-21 23:17:09 -0500

    Update `short.txt` and solution
    Added solution to part 2 to and updated `short.txt` with the list from
    the example case.

--- day3/binary_diagnostic.py
+++ day3/binary_diagnostic.py
@@ -1,6 +1,31 @@
 """
 Advent of Code 2021 Day 3
 """
+def find_most_and_least_common_bits(binary_strings, bit_position):
+    """
+    Finds the most and least common bits at a particular bit location throughout
+    a list of binary strings
+
+    Keyword arguments:
+    binary_strings -- a list of binary strings
+    bit_position -- the ith-position where the most common and least common
+        bits need to be searched
+    """
+    ones = zeros = 0
+    most_common_bit = None
+    least_common_bit = None
+    for binary in binary_strings:
+        if binary[bit_position] == '1':
+            ones += 1
+        else:
+            zeros += 1
+    if (ones > zeros) or (ones == zeros):
+        most_common_bit = 1
+        least_common_bit = 0
+    else:
+        most_common_bit = 0
+        least_common_bit = 1
+    return (most_common_bit, least_common_bit)
 
 # List holding counters for each bit
 ones_counter = [0 for i in range(12)]
@@ -26,26 +51,42 @@ gamma_rate = int(''.join(most_common_bits), 2)
 epsilon_rate = int(''.join(least_common_bits), 2)
 
 # Print out solution
+print("Part 1:")
 print(f"Gamma rate: {gamma_rate}")
 print(f"Epsilon rate: {epsilon_rate}")
 print(f"Power consumption: {gamma_rate * epsilon_rate}")
 
-# Solution to part 2
-most_common_bits = ''.join(most_common_bits)
-print(f"Most common bits: {most_common_bits}")
+"""
+Part 2
+"""
+# Make copies of the original report. We'll mutate these in place as we begin to
+# filter the numbers that match the criteria for the oxygen generator rating and
+# the co2 scrubber rating according to the puzzle description.
+o2_gen_list = report[:]
+co2_scrub_list = report[:]
 
-oxygen_generator_rating = 0
-for i in range(len(most_common_bits)):
-    if oxygen_generator_rating == 0:
-        print(f"\nIteration {i}")
-        for binary in report:
-            print(f"Comparing {most_common_bits[:-i or None]} with")
-            print(f"          {binary}")
-            if most_common_bits[:-i or None] == binary[:-i or None]:
-                print(f"Matched {binary}")
-                oxygen_generator_rating = binary
-                break
-            else:
-                    print(f"No match for {most_common_bits[:-i or None]}")
-    else:
-        break
+# Loop over all bits
+for i in range(len(report[0])):
+
+    # Determine the most and least common digits in the ith-bit
+    mc_bit, _ = find_most_and_least_common_bits(o2_gen_list, i)
+    _, lc_bit = find_most_and_least_common_bits(co2_scrub_list, i)
+
+    # Filter numbers with `bit` in bit position `i`
+    if len(o2_gen_list) > 1:
+        o2_gen_list[:] = [number for number in o2_gen_list
+                if int(number[i]) == mc_bit]
+    if len(co2_scrub_list) > 1:
+        co2_scrub_list[:] = [number for number in co2_scrub_list
+                if int(number[i]) == lc_bit]
+
+# Each list should have only 1 element at this point
+o2_generator_rating = o2_gen_list.pop()
+co2_scrubber_rating = co2_scrub_list.pop()
+
+print("\nPart 2:")
+print(f"O2 rating: {int(o2_generator_rating, 2)}")
+print(f"CO2 rating: {int(co2_scrubber_rating, 2)}")
+life_support_rating = int(o2_generator_rating, 2) * \
+                      int(co2_scrubber_rating, 2)
+print(f"Submarine life support rating: {life_support_rating}")
